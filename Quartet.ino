ac9d2c891f149b754c6a36be2bac75c495374e5f
Aritemis <arianafairbanks@gmail.com>
2017-12-13 11:35:20 -0700

    Notes updated

--- Quartet.ino
+++ Quartet.ino
@@ -1,7 +1,6 @@
 #include <Adafruit_CircuitPlayground.h>
 #include "pitches.h"
 #include <SoftwareSerial.h>
-#include <Wire.h>
 
 SoftwareSerial port(9, 10);
 boolean toggle = false;
@@ -11,7 +10,8 @@ int j = 0;
 int colorCounter = 0;
 int k = 0;
 
-int part1[] = {
+int part1[] = 
+{
   0, NOTE_F4, 0, NOTE_F4, 0, NOTE_F4, 0, NOTE_F4,
   0, NOTE_F4, 0, NOTE_F4, 0, NOTE_F4, 0, NOTE_F4, //measure 2
   0, NOTE_DS4, 0, NOTE_DS4, 0, NOTE_DS4, 0, NOTE_DS4,
@@ -43,10 +43,11 @@ int part1[] = {
   0, NOTE_DS4, 0, NOTE_DS4, 0, NOTE_DS4, 0, NOTE_DS4
 };
 
-int part2[] = {
+int part2[] = 
+{
   0, 0, 0, 0, 0, 0, 0, 0, // measure 1
-  0, 0, 0, 0, 0, 0, 
   NOTE_AS4, NOTE_AS5, NOTE_AS4, NOTE_AS5, // dur 1/16
+  0, 0, 0, 0, 0, 0, // measure 2
   0, 0, 0, 0, 0, 0, 0, 0, // measure 3
   0, 0, 0, 0, 0, 0, 0, 0, // measure 4
   0, 0, 0, 0, 
@@ -65,9 +66,7 @@ int part2[] = {
   NOTE_G5, // measure 14
   NOTE_GS5, 
   NOTE_A6, // 4 counts
-  NOTE_C5, 
-  NOTE_AS4, 
-  0, // measure 15 (inserted rest to complete measure)
+  0, NOTE_C5, NOTE_AS4, // measure 15
   NOTE_CS5, // 3 counts
   NOTE_C5, 
   NOTE_AS4, // 3 counts
@@ -76,16 +75,16 @@ int part2[] = {
   0, 0, 0, NOTE_DS5, // measure 17
   NOTE_CS5, // 3 counts
   NOTE_C5, NOTE_AS4, NOTE_C5, NOTE_CS5, // dur 1/16
-  0, NOTE_DS5, 0, // added rest to complete measure 18
+  NOTE_CS5, NOTE_DS5, 0, // measure 18
   NOTE_F5, 
   NOTE_DS5, // 3 counts
-  NOTE_CS5, 
-  NOTE_CS5, NOTE_C5, NOTE_CS5, NOTE_C5, // dur 1/16
+  NOTE_CS5, // 2 counts
+  NOTE_CS5, NOTE_C5, NOTE_CS5, NOTE_C5, // dur 1/16 measure 19
   NOTE_CS5, // 3 counts
-  NOTE_C5, NOTE_AS4, 
-  NOTE_AS4, NOTE_A4, NOTE_GS4, 0, // measure 20
+  NOTE_C5, NOTE_AS4, // 2 counts
+  NOTE_AS4, NOTE_A4, NOTE_GS4, // measure 20
   // {NOTE_AS4, NOTE_A4, NOTE_GS4} SPECIAL duration of 1/8 divided by 3
-  // I modified the last 3 notes to add a rest and "complete" measure 20
+  // I modified the last 3 notes 
   // by instead using each at 1/8 in part because 1/16 notes sound awful
   // and there's like, no point to making one even shorter; also timing?
   NOTE_G4, // 3 counts
@@ -97,13 +96,13 @@ int part2[] = {
   NOTE_C5, // 3 counts
   NOTE_CS5, 
   // measure 22
-  NOTE_F5, NOTE_DS5, 
+  NOTE_F5, NOTE_DS5, // 2 counts
   NOTE_DS5, // 3 counts
-  NOTE_DS5, NOTE_F5, // dur 1/16
-  0, 0, // added 2 rests to complete measure 23
-  NOTE_FS5, NOTE_F5, NOTE_DS5, 
-  NOTE_CS5, NOTE_DS5, // dur 1/16
-  0, 0, 0, 0, // added 4 rests to complete measure 24
+  NOTE_DS5, NOTE_F5, // dur 1/16 measure 23
+  NOTE_FS5, // 2 counts
+  NOTE_F5,
+  NOTE_DS5, // 2 counts
+  NOTE_CS5, NOTE_DS5, // dur 1/16 measure 24
   NOTE_C5, // 4 counts
   0, 0, 0, 
   NOTE_DS5, NOTE_F5, // dur 1/16 measure 25
@@ -124,9 +123,10 @@ int part2[] = {
   0, 0, 0, 0 // added 4 rests to complete measure 29
   };
 
-int part2dur[] = {
+int part2dur[] = 
+{
   42, 42, 42, 42, 42, 42, 42, 42, // measure 1
-  42, 42, 42, 42, 42, 42, 21, 21, 21, 21, // measure 2
+  21, 21, 21, 21, 42, 42, 42, 42, 42, 42, // measure 2
   42, 42, 42, 42, 42, 42, 42, 42, // measure 3
   42, 42, 42, 42, 42, 42, 42, 42, // measure 4
   42, 42, 42, 42, 21, 21, 21, 21, 42, 42, // measure 5
@@ -142,11 +142,12 @@ int part2dur[] = {
   125, 42, 125, 42, // measure 16
   167, 42, 42, 42, 42, // measure 17
   125, 21, 21, 21, 21, 42, 42, 42, // measure 18
-  42, 125, 42, 21, 21, 21, 21, 125, 42, 42, 42, 42, 42, 42, // measures 19 & 20
+  42, 125, 83, 21, 21, 21, 21, 
+  125, 83, 83, 14, 14, 14, // measures 20
   125, 167, 42, // measure 21
   125, 42, 125, 42, // measure 22
-  42, 42, 125, 21, 21, 42, 42, // measure 23
-  42, 42, 42, 21, 21, 42, 42, 42, 42, // measure 24
+  83, 83, 125, 21, 21, // measure 23
+  83, 42, 83, 21, 21, // measure 24
   167, 42, 42, 42, 21, 21, // measure 25
   125, 42, 125, 42, // measure 26
   83, 167, 42, 21, 21, // measure 27
@@ -154,21 +155,22 @@ int part2dur[] = {
   83, 83, 42, 42, 42, 42 // measure 29
   };
 
-int part3[] = {
+int part3[] = 
+{
   NOTE_AS3, NOTE_C4, NOTE_CS4, NOTE_AS3, NOTE_C4, NOTE_CS4, 
-  NOTE_AS3, NOTE_C4, // dur 1/16 
+  NOTE_AS3, NOTE_C4, // 
   NOTE_CS4, // measure 1 (8)
   NOTE_AS3, NOTE_C4, NOTE_CS4, NOTE_AS3, NOTE_C4, NOTE_CS4, 
-  NOTE_AS3, NOTE_C4, // dur 1/16 @ 15 & 16
+  NOTE_AS3, NOTE_C4, // 
   NOTE_CS4, // measure 2 (17)
   NOTE_A3, NOTE_AS3, NOTE_C4, NOTE_A3, NOTE_AS3, NOTE_C4, 
-  NOTE_GS3, NOTE_AS3, // dur 1/16 @ 24 & 25
+  NOTE_GS3, NOTE_AS3, // 
   NOTE_C4, // measure 3 (26)
   NOTE_A3, NOTE_AS3, NOTE_C4, NOTE_A3, NOTE_AS3, NOTE_C4, 
-  NOTE_GS3, NOTE_AS3, // dur 1/16 @ 33 & 34
+  NOTE_GS3, NOTE_AS3, // 
   NOTE_C4, // measure 4 (35)
   NOTE_AS3, NOTE_C4, NOTE_CS4, NOTE_AS3, NOTE_C4, NOTE_CS4, 
-  NOTE_AS3, NOTE_C4, // dur 1/16 @ 42 & 43
+  NOTE_AS3, NOTE_C4, // 
   NOTE_CS4, 
   NOTE_AS3, NOTE_C4, NOTE_CS4, NOTE_AS3, NOTE_C4, NOTE_CS4, 
   NOTE_AS3, NOTE_C4, // dur 1/16
@@ -241,7 +243,8 @@ int part3[] = {
   NOTE_C4
   };
 
-int part4[] = {
+int part4[] = 
+{
   0, 0, 0, 0, 0, 0, 0, 0, 
   0, 0, 0, 0, 0, 0, 0, 0, // measure 2
   0, 0, 0, 0, 0, 0, 0, 0, 
@@ -255,11 +258,11 @@ int part4[] = {
   0, 0, 0, 0, 0, 0, NOTE_FS4, NOTE_F4, 
   0, 0, 0, 0, 0, 0, NOTE_F4, NOTE_FS4, // measure 12
   0, NOTE_DS4, NOTE_F4, NOTE_FS4, NOTE_F4, NOTE_FS4, NOTE_FS4, NOTE_F4, 
-  NOTE_AS4, //3 counts @104
+  NOTE_AS4, //3 counts 
   NOTE_BS4,
-  NOTE_G4, //3 counts @106
+  NOTE_G4, //3 counts 
   NOTE_G4, // measure 14
-  NOTE_GS4, NOTE_A4, NOTE_A4, //4 counts @110
+  NOTE_GS4, NOTE_A4, NOTE_A4, //4 counts 
   0, 0, // measure 15 (112)
   0, 0, 0, 0, 0, 0, 0, 0, // measure 16 (120)
   0, 0, 0, 0, 0, 0, 0, 0, 
@@ -272,18 +275,36 @@ int part4[] = {
   0, 0, 0, 0, 0, 0, 0, 0, // measure 24 (184)
   0, 0, 0, 0, 0, 0, 0, 0, // measure 25 (192)
   0, 0, 
-  NOTE_AS4, //2 counts @195
-  NOTE_AS4, //4 counts @196 -- measure 26
+  NOTE_AS4, //2 counts
+  NOTE_AS4, //4 counts 
   0, 0, 
-  NOTE_FS4, //4 counts @199
+  NOTE_FS4, //4 counts 
   0, 0, 
   0, 0, 
-  NOTE_FS4, //4 counts @204
+  NOTE_FS4, //4 counts 
   0, 0, 
-  NOTE_F4, //4 counts @207
-  NOTE_DS4, //2 counts @208
-  NOTE_C4 //2 counts @209
+  NOTE_F4, //4 counts 
+  NOTE_DS4, //2 counts 
+  NOTE_C4 //2 counts 
 };
+
+void playNote(int mod, int current[], int noteDuration, int maxSize) 
+{
+  noteDuration *= mod;
+  CircuitPlayground.playTone(current[i], noteDuration);
+  if (i < maxSize) i++;
+  else
+  {
+    toggle = false;
+    i = 0;
+    p = 5;
+  }
+  CircuitPlayground.setPixelColor(random(0,9), random(0,255), random(0,255), random(0,255));
+  if (j < 10) j++;
+  else j = 0;
+  delay(100);
+  CircuitPlayground.setPixelColor(j-1, 0, 0, 0);
+}
 
 void setup() 
 {
@@ -342,8 +363,12 @@ void loop()
     if(p == 1)
     {
       port.write('b');
-      int maxSize = sizeof(part1);
-      playNote(2, part1, 42, maxSize);
+      int duration = 42;
+      if (i == 195 || i == 208 || i == 209) duration = 83; // 2 counts
+      else if (i == 104 || i == 106) duration = 125; // 3 counts
+      else if (i == 110 || i == 196 || i == 199 || i == 204 || 207) duration = 167; // 4 counts
+      int maxSize = sizeof(part4);
+//      playNote(2, part4, duration, maxSize);
     }
     else if(p == 2)
     {
@@ -355,6 +380,11 @@ void loop()
     else if(p == 3)
     {
       port.write('d');
+      int maxSize = sizeof(part1);
+      playNote(2, part1, 42, maxSize);
+    }
+    else if(p == 4)
+    {
       int duration = 42;
       int maxSize = sizeof(part3);
       if(k == 6 || k == 7) duration = 21;
@@ -365,33 +395,6 @@ void loop()
         k = 0;
       }
     }
-    else if(p == 4)
-    {
-      int duration = 42;
-      if (i == 195 || i == 208 || i == 209) duration = 83; // 2 counts
-      else if (i == 104 || i == 106) duration = 125; // 3 counts
-      else if (i == 110 || i == 196 || i == 199 || i == 204 || 207) duration = 167; // 4 counts
-      int maxSize = sizeof(part4);
-//      playNote(2, part4, duration, maxSize);
-    }
   }
-}
-
-void playNote(int mod, int current[], int noteDuration, int maxSize) 
-{
-  noteDuration *= mod;
-  CircuitPlayground.playTone(current[i], noteDuration);
-  if (i < maxSize) i++;
-  else
-  {
-    toggle = false;
-    i = 0;
-    p = 5;
-  }
-  CircuitPlayground.setPixelColor(random(0,9), random(0,255), random(0,255), random(0,255));
-  if (j < 10) j++;
-  else j = 0;
-  delay(100);
-  CircuitPlayground.setPixelColor(j-1, 0, 0, 0);
 }
 
